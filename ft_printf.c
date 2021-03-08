981d148c3db9e6a5c6be31777c4b78114b1d7209
kypark <minequinto2@gmail.com>
2021-03-08 19:25:45 +0900

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -23,21 +23,25 @@ void	ft_reset_list(void)
 
 void	ft_ajust(void)
 {
+	// flag가 . 이고 prec 가 < 0 일때
 	if (g_plist.flags[0] == 3 && g_plist.prec[0] < 0)
 	{
 		g_plist.flags[0] = 0;
 		g_plist.prec[0] = 0;
 	}
+	//flag가 0 이고 prec가 <0 일때
 	else if (g_plist.flags[0] == 2 && g_plist.prec[0] < 0)
 	{
 		g_plist.flags[0] = 1;
 		g_plist.prec[0] *= -1;
 	}
+	//prec 가 <0 일때
 	else if (g_plist.prec[0] < 0)
 	{
 		g_plist.flags[0] = 1;
 		g_plist.prec[0] *= -1;
 	}
+	//두번째 플레그가 있고 두번째 pre <0 일때
 	if (g_plist.flags[1] && g_plist.prec[1] < 0)
 	{
 		g_plist.flags[1] = 0;
@@ -53,23 +57,23 @@ int		ft_printf(const char *format, ...)
 	va_start(g_plist.arg, format);
 	while (*format)
 	{
-		ft_reset_list();
-		if (*format == '%')
+		ft_reset_list(); //구조체 초기화
+		if (*format == '%') //%이면
 		{
-			format++;
-			ft_get_flag(&format);
+			format++; 
+			ft_get_flag(&format); //ft_get.c 플래그 
 			ft_ajust();
-			sum += ft_get_type(&format);
+			sum += ft_get_type(&format); //타입 넣기
 		}
 		else
 		{
 			ft_putchar(*format);
 			sum++;
 		}
-		if (g_plist.error)
+		if (g_plist.error) //만약에러가 있으면
 			return (-1);
-		format++;
+		format++;//증가
 	}
-	va_end(g_plist.arg);
-	return (sum);
+	va_end(g_plist.arg); //마지막
+	return (sum); //길이값 넘겨주기
 }
