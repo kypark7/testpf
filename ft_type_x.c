f58b8cb14264ce697ea9006bfdcdc09c656e480a
kypark <minequinto2@gmail.com>
2021-03-09 14:42:56 +0900

    Update ft_type_x.c

--- ft_type_x.c
+++ ft_type_x.c
@@ -18,6 +18,7 @@ int			ft_flags_before(int size, int i, int dot)
 	if (g_plist.flags[0] == 2)
 		while (-dot + i++ < g_plist.prec[0] - size)
 		{
+			//앞 공백
 			if (g_plist.flags[1])
 				write(1, " ", 1);
 			else
@@ -51,13 +52,16 @@ int			ft_type_x_dot(unsigned int n, char *base, char *res)
 	i = ft_flags_before(size, i, dot);
 	while (j++ < size - x_i)
 		write(1, "0", 1);
+	//res hex 저장
 	while (x_i--)
 	{
 		res[x_i] = base[n % 16];
 		n /= 16;
 	}
+	//!n = true and !g_plist.pre[1] = true
 	dot ? size-- : ft_putstr(res);
 	free(res);
+	//뒤에 ' '
 	if (g_plist.flags[0] == 1)
 		while (i++ < g_plist.prec[0] - size)
 			write(1, " ", 1);
@@ -81,6 +85,7 @@ int			ft_type_x(unsigned int n, int i, char *base)
 	//!n = true andd 첫번째 플래그 . and 처번째 prec 없 ==> dot
 	dot = !n && g_plist.flags[0] == 3 && !g_plist.prec[0] ? 1 : 0;
 	i = ft_flags_before(size, i, dot);
+	//res에 hex해서 저장
 	while (x_i--)
 	{
 		res[x_i] = base[n % 16];
